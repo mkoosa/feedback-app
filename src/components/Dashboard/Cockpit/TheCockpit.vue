f02e60ee2a96b6f9953dd58063d24c2b8a688683
Marcin Kosecki <marcin.kosecki@int.pl>
2024-02-23 15:04:30 +0100

    Add feedback btn to  TheCockpit component

--- src/components/Dashboard/Cockpit/TheCockpit.vue
+++ src/components/Dashboard/Cockpit/TheCockpit.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="dashboard__cockpit cockpit indent">
     <CockpitSort />
+    <TheButtonVue @@eventFromBtn="click" element="button" bgc="lila" text="add feedback"
+      >+</TheButtonVue
+    >
   </div>
 </template>
 
 <script setup lang="ts">
 import CockpitSort from './CockpitSort.vue';
+import TheButtonVue from '@/components/Shared/TheButton.vue';
+
+const click = (): void => console.log('ok');
 </script>
 
 <style scoped>
 .cockpit {
-  padding-top: 2rem;
-  padding-bottom: 2rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
   color: var(--white);
   background: var(--dark-blue);
   display: flex;
   align-items: center;
+  justify-content: space-between;
 }
 </style>
--- src/components/Shared/TheButton.vue
+++ src/components/Shared/TheButton.vue
@@ -1,3 +1,61 @@
 <template>
-  <div></div>
+  <button
+    @click="$emit('@eventFromBtn')"
+    v-if="element === 'button'"
+    :bgc="bgc"
+    type="button"
+    class="btn"
+    :class="btnClass"
+  >
+    <slot></slot>
+    {{ text }}
+  </button>
+  <button v-if="element === 'submit'" type="submit" class="btn" :bgc="bgc" :class="btnClass">
+    {{ text }}
+  </button>
 </template>
+
+<script setup lang="ts">
+import { toRefs, computed } from 'vue';
+const props = defineProps<{
+  element: string;
+  bgc: string;
+  text: string;
+}>();
+
+const { bgc } = toRefs(props);
+
+const btnClass = computed(() => bgc.value);
+
+defineEmits<{
+  (e: '@eventFromBtn'): void;
+}>();
+</script>
+
+<style scoped>
+.btn {
+  padding: 0.85em 1.1em;
+  background: #000;
+  font-size: 1.3rem;
+  font-weight: 600;
+  letter-spacing: 0.05rem;
+  color: white;
+  outline: none;
+  border: none;
+
+  text-transform: capitalize;
+  border-radius: 1rem;
+}
+
+.lila {
+  background: var(--lila);
+}
+
+.blue {
+  background: var(--blue);
+}
+
+.dark-blue {
+  background: var(--dark-blue);
+}
+</style>
